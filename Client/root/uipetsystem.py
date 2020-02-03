0313d0f1f85926de197513125e80307c5fd8ec23
Robert <55710848+Rakancito@users.noreply.github.com>
2020-02-02 18:29:00 -0600

    Create uipetsystem.py

---
+++ Client/root/uipetsystem.py
@@ -0,0 +1,10 @@
+# Search
+
+			PET_FILE_NAME = "%s/pet_skill.txt" % rgcapplic.GetLocalePath()
+			PET_FILE_SKILL = "%s/pet_skill_bonus.txt" % rgcapplic.GetLocalePath()
+      
+      
+# Replace for:
+
+      PET_FILE_NAME = "%s/pet_skill.txt" % app.GetMyLang()
+      PET_FILE_SKILL = "%s/pet_skill_bonus.txt" % app.GetMyLang()
