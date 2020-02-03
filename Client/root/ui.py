7d2235f1cac09acb944c00dfd007320b30ebde8f
Robert <55710848+Rakancito@users.noreply.github.com>
2020-02-02 18:24:46 -0600

    Create ui.py

---
+++ Client/root/ui.py
@@ -0,0 +1,8 @@
+# Search in def LoadScriptFile(self, window, FileName):
+
+self.ScriptDictionary["LOCALE_PATH"] =
+
+# Replace all line for:
+
+self.ScriptDictionary["LOCALE_PATH"] = app.GetMyLang()
+
