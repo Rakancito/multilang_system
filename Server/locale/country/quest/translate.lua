929d43c28bb08ee86ac0c2addc2a2460c27aeaf5
Robert <55710848+Rakancito@users.noreply.github.com>
2020-02-02 18:15:19 -0600

    Create translate.lua

---
+++ Server/locale/country/quest/translate.lua
@@ -0,0 +1,11 @@
+["en"]gameforge.collect_quest_lv30._140_npcChat = "Orc teeth "
+["en"] = "Biologist Chaegirab: "
+["en"] = "I'm so sorry, but I haven't finished [ENTER] the analysis of the last Tooth yet. [ENTER] Can you come later?"
+
+["es"]gameforge.collect_herb_lv10._50_sayTitle = "Biólogo Chaegirab: "
+["es"]gameforge.collect_quest_lv30._140_npcChat = "Dientes de Orco "
+["es"]gameforge.collect_quest_lv30._200_say = "Lo siento mucho, pero todavía no he terminado[ENTER]el análisis del último Diente.[ENTER]¿Puedes venir más tarde? "
+
+["hu"] = "Biológus Chaegirab: "
+["hu"] = "Sajnálom, de még nem fejeztem be az [ENTER] elemzését az utolsó fogról. [ENTER] Tudsz később jönni?"
+["hu"]gameforge.collect_quest_lv30._140_npcChat = "Ork fogak "
