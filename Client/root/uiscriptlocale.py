ef77501cefe55e18b47891689fe400ae40be8309
Robert <55710848+Rakancito@users.noreply.github.com>
2020-02-02 18:30:00 -0600

    Create uiscriptlocale.py

---
+++ Client/root/uiscriptlocale.py
@@ -0,0 +1,9 @@
+# Search
+
+	elif "EUROPE" == rgcapplic.GetLocaleServiceName():
+		name = rgcapplic.GetLocalePath()
+    
+# Replace for
+
+	elif "EUROPE" == app.GetLocaleServiceName():
+		name = app.GetMyLang()
