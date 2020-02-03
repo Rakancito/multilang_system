2662ac73debd958c87aee0aa7ce6ebd222946160
Robert <55710848+Rakancito@users.noreply.github.com>
2020-02-02 18:16:59 -0600

    Update translate.lua

--- Server/locale/country/quest/translate.lua
+++ Server/locale/country/quest/translate.lua
@@ -1,11 +1,13 @@
+-- You just need continue the logic ^^
+
+["en"]gameforge.collect_herb_lv10._50_sayTitle = "Biologist Chaegirab: "
 ["en"]gameforge.collect_quest_lv30._140_npcChat = "Orc teeth "
-["en"] = "Biologist Chaegirab: "
-["en"] = "I'm so sorry, but I haven't finished [ENTER] the analysis of the last Tooth yet. [ENTER] Can you come later?"
+["en"]gameforge.collect_quest_lv30._200_say = "I'm so sorry, but I haven't finished [ENTER] the analysis of the last Tooth yet. [ENTER] Can you come later?"
 
 ["es"]gameforge.collect_herb_lv10._50_sayTitle = "Biólogo Chaegirab: "
 ["es"]gameforge.collect_quest_lv30._140_npcChat = "Dientes de Orco "
 ["es"]gameforge.collect_quest_lv30._200_say = "Lo siento mucho, pero todavía no he terminado[ENTER]el análisis del último Diente.[ENTER]¿Puedes venir más tarde? "
 
-["hu"] = "Biológus Chaegirab: "
-["hu"] = "Sajnálom, de még nem fejeztem be az [ENTER] elemzését az utolsó fogról. [ENTER] Tudsz később jönni?"
+["hu"]gameforge.collect_herb_lv10._50_sayTitle = "Biológus Chaegirab: "
 ["hu"]gameforge.collect_quest_lv30._140_npcChat = "Ork fogak "
+["hu"]gameforge.collect_quest_lv30._200_say = "Sajnálom, de még nem fejeztem be az [ENTER] elemzését az utolsó fogról. [ENTER] Tudsz később jönni?"
